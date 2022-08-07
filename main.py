681d0aa6fd5695d75153405eac4706a07f758597
Jeff-Adler <jeffm.adler@gmail.com>
2022-08-07 16:44:42 -0400

    fix helper method to check for out of bounds ids

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -6,10 +7,17 @@ fake_notes_db = ["note_1", "note_2", "note_3"]
 class Note(BaseModel):
 	contents: str
 
+# helper functions
+def is_id_out_of_bounds(some_id: int, some_list: list) -> bool:
+	return some_id > len(some_list) - 1 or some_id < 0
+
+log = logging.getLogger("JournalApi")
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
+	log.info("gar")
 	return {"message" : "Welcome to the Journal App!"}
 
 @app.get("/notes")
@@ -18,7 +26,8 @@ async def get_notes():
 
 @app.get("/notes/{note_id}")
 async def get_note_by_id(note_id: int):
-	if note_id > len(fake_notes_db) - 1 or note_id < 0:
+	log.info("gar")
+	if is_id_out_of_bounds(note_id, fake_notes_db):
 		return {"error" : "note_id out of bounds"}
 	return {note_id: fake_notes_db[note_id]}
 
@@ -26,3 +35,7 @@ async def get_note_by_id(note_id: int):
 async def create_note(note: Note):
 	fake_notes_db.append(note.contents)	
 	return note.contents
+
+# @app.delete("/notes/{note_id}")
+# async def delete_note_by_id(note_id: int):
+# 	if is_id_in_bounds(note_id, fake_notes_db):
