ecc8dd7cd78df801589fd6fb5942f4257b4a05f7
Jeff-Adler <jeffm.adler@gmail.com>
2022-08-07 16:13:04 -0400

    wrote first POST route

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 fake_notes_db = ["note_1", "note_2", "note_3"]
+
+class Note(BaseModel):
+	contents: str
 
 app = FastAPI()
 
@@ -16,4 +20,9 @@ async def get_notes():
 async def get_note_by_id(note_id: int):
 	if note_id > len(fake_notes_db) - 1 or note_id < 0:
 		return {"error" : "note_id out of bounds"}
-	return {note_id: fake_notes_db[note_id]}	
+	return {note_id: fake_notes_db[note_id]}
+
+@app.post("/notes")
+async def create_note(note: Note):
+	fake_notes_db.append(note.contents)	
+	return note.contents
