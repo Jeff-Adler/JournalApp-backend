9cfc8a78e8c189b57737177a15365ceec9545f2d
Jeff-Adler <jeffm.adler@gmail.com>
2022-08-07 15:36:25 -0400

    added first GET routes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
+SAMPLE_NOTES = ["note_1", "note_2", "note_3", "note_4", "note_5"]
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
 	return {"message" : "Welcome to the Journal App!"}
+
+@app.get("/")
+async def get_notes():
+	return {"notes": "populate this with notes"}
+
+@app.get("/")
+async def get_notes():
+	return {"notes": "populate this with notes"}	
