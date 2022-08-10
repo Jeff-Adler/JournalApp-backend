f9669bf426c6da252638e29a758ff940e5dbc8dd
Jeff-Adler <jeffm.adler@gmail.com>
2022-08-09 21:31:28 -0400

    finished database file

--- database.py
+++ database.py
@@ -3,5 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Connect to SQLite db
+SQLALCHEMY_DATABASE_URL = "sqlite:///./JournalApp-backend.db"
+# Connect to PostgreSQL db (INSERT POSTGRES DB URL)
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-# Connect to PostgreSQL db
+# Create SQLAlchemy engine (connect args argument is SQLite specific)
+engine = create_engine(
+	SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
