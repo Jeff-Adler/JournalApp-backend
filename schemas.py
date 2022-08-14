6dc49fc1ad95f71f052e9e433adbdb5b0455c15e
Jeff-Adler <jeffm.adler@gmail.com>
2022-08-13 21:55:05 -0400

    disambiguated schemas

--- schemas.py
+++ schemas.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel
 
+# Defines attributes that all schemas will inherit for all actions (reading/writing, etc.)
 class UserBase(BaseModel):
 	email: str
 
+# Used for creating items
 class UserCreate(UserBase):
 	pass
 
+# Used for reading items
 class User(UserBase):
 	pass
 
