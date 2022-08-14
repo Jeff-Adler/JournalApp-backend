2a482efb5610cffead7a55dc8a90bf1ed881349c
Jeff-Adler <jeffm.adler@gmail.com>
2022-08-13 22:15:22 -0400

    finished writing schemas

--- schemas.py
+++ schemas.py
@@ -6,11 +6,19 @@ class UserBase(BaseModel):
 
 # Used for creating items
 class UserCreate(UserBase):
-	pass
+	password: str
 
 # Used for reading items
 class User(UserBase):
-	pass
+	id: int
+	is_active: bool
+
+	todos: List[Todo] = []
+
+	# This allows you to set certain config options with Pydantic
+    class Config:
+    	# allows you to do e.q. current_user.items as a shorthand for data.id or relationship attributes like current_user.items
+    	orm_mode = True
 
 class TodoBase(BaseModel):
 	task: str
@@ -19,4 +27,9 @@ class TodoCreate(TodoBase):
 	pass
 
 class Todo(TodoBase):
-	pass
+	id: int
+	is_done: bool
+	owner_id: int
+
+	class Config:
+		orm_mode = true
